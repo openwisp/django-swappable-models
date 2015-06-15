6166acf96ac7b36b7b103a44dff3d2d7ba7751f2
S. Andrew Sheppard <andrew@wq.io>
2015-06-15 15:14:49 -0500

    v0.2.2

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def parse_markdown_readme():
 
 setup(
     name='swapper',
-    version='0.2.1',
+    version='0.2.2',
     author='S. Andrew Sheppard',
     author_email='andrew@wq.io',
     url='https://github.com/wq/django-swappable-models',
@@ -52,6 +52,7 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
     tests_require=['django>=1.6'],
