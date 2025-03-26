47684ae44d0f6f42ff893fbcce7c7a2bcae51ad2
Gagan Deep <pandafy.dev@gmail.com>
2025-03-26 22:10:41 +0530

    [deps] Added support for Django >=5.1,<5.3 and Python >=3.12, 3.14 #55
    - Dropped support for Python < 3.9
    - Dropped support for Django < 4.2
    
    Closes #55

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,13 +7,8 @@ from django.test.utils import setup_test_environment
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', "tests.settings")
 setup_test_environment()
 
-if hasattr(django, 'setup'):
-    # Django 1.7+
-    django.setup()
-    call_command('makemigrations', 'default_app', interactive=False)
-    if os.environ["DJANGO_SETTINGS_MODULE"] == "tests.swap_settings":
-        call_command('makemigrations', 'alt_app', interactive=False)
-    call_command('migrate', interactive=False)
-else:
-    # Django 1.6
-    call_command('syncdb', interactive=False)
+django.setup()
+call_command('makemigrations', 'default_app', interactive=False)
+if os.environ["DJANGO_SETTINGS_MODULE"] == "tests.swap_settings":
+    call_command('makemigrations', 'alt_app', interactive=False)
+call_command('migrate', interactive=False)
