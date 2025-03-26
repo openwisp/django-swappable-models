854c289901133e78cb9a6ab4be42ebf52e9d134b
Gagan Deep <pandafy.dev@gmail.com>
2025-03-26 22:10:41 +0530

    [deps] Added support for Django >=5.1,<5.3 and Python >=3.12, 3.14 #55
    - Dropped support for Python < 3.9
    - Dropped support for Django < 4.2
    
    Closes #55

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ The unofficial Django swappable models API.
 
 def readme():
     try:
-        readme = open('README.md')
+        with open('README.md', encoding='utf-8') as f:
+            return f.read()
     except IOError:
         return LONG_DESCRIPTION
-    else:
-        return readme.read()
 
 
 setup(
     name='swapper',
     use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     author='S. Andrew Sheppard',
     author_email='andrew@wq.io',
     url='https://github.com/openwisp/django-swappable-models',
@@ -31,23 +31,17 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Framework :: Django',
-        'Framework :: Django :: 2.0',
-        'Framework :: Django :: 2.1',
-        'Framework :: Django :: 2.2',
-        'Framework :: Django :: 3.0',
-        'Framework :: Django :: 3.1',
-        'Framework :: Django :: 3.2',
-        'Framework :: Django :: 4.0',
         'Framework :: Django :: 4.2',
         'Framework :: Django :: 5.0',
+        'Framework :: Django :: 5.1',
+        'Framework :: Django :: 5.2',
     ],
-    tests_require=['django>=2.0'],
-    test_suite='tests',
-    setup_requires=['setuptools_scm'],
+    tests_require=['django>=4.2'],
+    python_requires='>=3.9',
 )
